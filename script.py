fd92a9221ad8090fee15b8f9ccc256ed96474b60
leniks <anciborko04@mail.ru>
2023-07-10 03:48:19 +0300

    get запрос

---
+++ script.py
@@ -0,0 +1,17 @@
+import requests
+from acc_token import token
+import pprint
+
+api_ver = 5.131
+user_id = 380060730
+
+data = requests.get('https://api.vk.com/method/friends.get', params={
+                            'access_token': token,
+                            'v': api_ver,
+                            'user_id': user_id,
+                            'order': 'name',
+                            'fields': 'bdate, can_post, can_see_all_posts, can_write_private_message, city, contacts,'
+                                      'country, domain, education, timezone, has_mobile, last_seen, nickname, online,'
+                                      'relation, sex, status, universities'
+                        }).json()['response']['items']
+pprint.pprint(data)
