437716fae2d34744aec7abfcec4cda1531ece8fb
leniks <anciborko04@mail.ru>
2023-07-11 02:10:35 +0300

    infowindow

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QScrollArea, QFormLayout, QGroupBox, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QScrollArea, QFormLayout, QGroupBox, QLabel, \
+    QVBoxLayout, QLineEdit, QMainWindow
 import json
 
 
@@ -39,14 +40,26 @@ class MainWindow(QWidget):
 
         self.setLayout(layout)
 
-
     def get_info(self):
         sender = self.sender()
         id = sender.text().split()[3]
         for elem in data:
             if str(elem['id']) == id:
                 person = elem
-                print(person)
+                self.infowindow = InfoWindow(elem)
+                self.infowindow.show()
+
+
+class InfoWindow(QMainWindow):
+    def __init__(self, elem):
+        super().__init__()
+        self.data = data
+        self.resize(400, 400)
+
+        self.textbox = QLineEdit()
+        self.setCentralWidget(self.textbox)
+
+        self.textbox.setText(str(elem['id']))
 
 
 app = QApplication(sys.argv)
